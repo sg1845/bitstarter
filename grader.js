407bec3a67d651b5d0f961da27daf10344398b4e
Sharon Guan <sg1845@att.com>
2013-07-08 15:10:15 -0500

    Update grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://fierce-reaches-1073.herokuapp.com";
+var rest = require('restler');
+var sys = require('util');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,9 +39,23 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(urlfile) {
+    urlfile = urlfile || URLFILE_DEFAULT; 
+    rest.get(urlfile).on('complete', function(result) {
+    if( result instanceof Error ) {
+        console.log("%s does not exist. Exiting.", urlfile);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    else {
+       return urlfile; 
+    }
+})
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -63,8 +80,9 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)   
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to URL', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
