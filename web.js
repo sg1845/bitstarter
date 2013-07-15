100d94976d2aa3dd48d7d6f2f553f96aacf00e0d
Sharon Guan <sg1845@att.com>
2013-07-15 06:41:12 -0500

    Fourth commit

--- web.js
+++ web.js
@@ -1,18 +1,16 @@
 
 var fs = require('fs');
-
-var contents = fs.readFileSync('index.html').toString(); 
-console.log(contents);
-
+var htmlfile = "index.html";
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- // response.send('Hello World!');
-  response.send(contents);
+  var html = fs.readFileSunc(htmlfile).toString();
+  response.send(html);
 });
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
   console.log("Listening on " + port);
